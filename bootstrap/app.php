5e2b04baa8560bde143607ee6427bbf9ef93ca59
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-09-28 17:55:23 +0000

    Updated files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use App\Http\Middleware\AdminRole;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+
+        //Add middleware aliases here
+        $middleware->alias([
+            'roles' => AdminRole::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
