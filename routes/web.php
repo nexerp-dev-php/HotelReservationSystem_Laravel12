a8350a1f8d70473f9eb69968b59681920b39df2f
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-09-30 00:45:22 +0000

    Added with custom logout handling

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,6 @@ require __DIR__.'/auth.php';
 //Admin group middleware
 Route::middleware(['auth', 'roles:admin'])->group(function() {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 });
 
