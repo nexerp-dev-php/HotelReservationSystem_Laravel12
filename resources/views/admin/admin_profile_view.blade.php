1dc648511a013734d3db3c22bf348d427050b563
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-10-04 20:17:46 +0800

    Updated with reflect canvas with selected image

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,11 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+<script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+
 			<div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -95,7 +100,7 @@
 												<h6 class="mb-0">Photo</h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<input type="file" class="form-control" name="photo" />
+												<input type="file" class="form-control" name="photo" id="image" />
 											</div>
 										</div>
 										<div class="row mb-3">
@@ -103,7 +108,7 @@
 												<h6 class="mb-0"></h6>
 											</div>
 											<div class="col-sm-9 text-secondary">
-												<img src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/',$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
+												<img id="showImage" src="{{ (!empty($profileData->photo)) ? url('upload/admin_images/',$profileData->photo) : url('upload/no_image.jpg') }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="80">
 											</div>
 										</div>										
 										<div class="row">
@@ -119,5 +124,17 @@
 					</div>
 				</div>
 			</div>
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#image').change(function(e) {
+		var reader = new FileReader();
+		reader.onload = function(e) {
+			$('#showImage').attr('src', e.target.result);
+		}
+		reader.readAsDataURL(e.target.files[0]);
+	});
+});
+</script>
 
 @endsection
