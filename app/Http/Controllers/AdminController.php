f33db763dbdcdcefef39d936a5a52e69e40bafb1
nexerp-dev-php <nexerp.dev.php@gmail.com>
2025-10-06 08:37:09 +0800

    Added with change password

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -59,5 +60,41 @@ class AdminController extends Controller
         );
 
         return redirect()->back()->with($notification);
-    }       
+    }   
+    
+    public function AdminChangePassword() {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+
+        return view('admin.admin_profile_change_password', compact('profileData'));
+    }
+
+    public function AdminPasswordUpdate(Request $request) {
+        //Validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)) {
+            $notification = array(
+                'message' => 'Old Password does not match!',
+                'alert-type' => 'error'
+            );
+
+            return back()->with($notification);
+        }
+
+        //Update the password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password updated successfully',
+            'alert-type' => 'success'
+        );
+
+        return back()->with($notification);
+    }
 }
